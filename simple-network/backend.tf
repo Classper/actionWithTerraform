82ec084d65ce0de72842cd4c360773a0668a58a0
Casper Dijkstra <cdijkstra@MacBook-Pro.local>
2020-08-31 16:09:24 +0200

    Created terraform networking module

--- simple-network/backend.tf
+++
@@ -1,9 +0,0 @@
-# configure backend to keep terraform state
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-norbi-terraform"
-    storage_account_name = "norbisaterraform"
-    container_name       = "norbitfstate"
-    key                  = "terraform.tfstate"
-  }
-}
